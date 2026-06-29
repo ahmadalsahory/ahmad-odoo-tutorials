d0c545d0b7b77cd0c274646182d46723edd1fd73
Ahmad Hammad <ahmad.hammad@artikeys.com>
2026-06-29 14:44:34 +0300

    Add estate_property model

---
+++ tutorials/estate/models/estate_property.py
@@ -0,0 +1,22 @@
+from odoo import fields, models
+
+class EstateProperty(models.Model):
+    _name = "estate.property"
+    _description = "Estate Property"
+
+    name = fields.Char(string="Title", required=True)
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date(copy=False, default=fields.Date.add(fields.Date.today(), months=3))
+    expected_price = fields.Float(required=True)
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
+    living_area = fields.Integer(string="Living Area (sqm)")
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer(string="Garden Area (sqm)")
+    garden_orientation = fields.Selection(
+        string="Garden Orientation",
+        selection=[("north", "North"), ("south", "South"), ("east", "East"), ("west", "West")],
+    )
